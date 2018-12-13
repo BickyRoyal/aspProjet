6dfaa33cc7b28784eda51e350d1fec7030bdf83e
BickyRoyal <vivaplagias@gmail.com>
2018-12-13 14:05:28 +0100

    finished project
    project ended

---
+++ SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+namespace SportsStore.Models {
+    public class EFOrderRepository : IOrderRepository {
+        private ApplicationDbContext context;
+        public EFOrderRepository(ApplicationDbContext ctx) {
+            context = ctx;
+        }
+        public IQueryable<Order> Orders => context.Orders
+            .Include(o => o.Lines)
+            .ThenInclude(l => l.Product);
+        public void SaveOrder(Order order) {
+            context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0) {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+        }
+        public Product DeleteProduct(int productID) {
+            Product dbEntry = context.Products
+            .FirstOrDefault(p => p.ProductID == productID);
+            if (dbEntry != null) {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+    }
+}
